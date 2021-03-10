299f6c7d3d753dca614d01d0fa095bae6f00de8b
epociask <epociask@luc.id>
2021-03-09 17:06:03 -0800

    [main] added cors

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -8,5 +8,9 @@ import (
 
 // HealthCheck handle health check
 func (bh *BookClubHandler) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+	w.Header().Add("Access-Control-Allow-Headers", "*")
 	render.JSON(w, r, bh.service.CheckHealth())
 }
