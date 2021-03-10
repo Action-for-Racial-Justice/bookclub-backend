adcae5a50e68ceb078edd928ddf6620aad29c961
Meghan Johnson <mjohnson1307@gmail.com>
2021-03-10 18:29:35 -0500

    Implements backend changes to handle CORS using go-chi/cors

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/google/wire"
+	"github.com/go-chi/cors"
 )
 
 //Module to denote wire binding function
@@ -33,6 +34,17 @@ func New(service service.Service) (*BookClubHandler, error) {
 	handlers := &BookClubHandler{service: service}
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
+
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	  }))
 
 	registerEndpoint("/health", router.Get, handlers.HealthCheck)
 	handlers.router = router
