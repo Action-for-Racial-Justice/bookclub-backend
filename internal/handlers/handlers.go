27c493e285fe72771c1026b842f1ab44b45ff683
Meghan Johnson <mjohnson1307@gmail.com>
2021-03-22 18:21:52 -0400

    Fixes go format error

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -53,18 +53,18 @@ func registerEndpoint(endpoint string, routeMethod func(pattern string, handlerF
 	routeMethod(endpoint, http.HandlerFunc(handlerFunc).ServeHTTP)
 }
 
-//setCorsOptions acts as a setter function for the cors.Options struct 
+//setCorsOptions acts as a setter function for the cors.Options struct
 func setCorsOptions() cors.Options {
 	corsOptions := cors.Options{
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedOrigins: []string{"https://*", "http://*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	  }
+	}
 
-	  return corsOptions
+	return corsOptions
 }
