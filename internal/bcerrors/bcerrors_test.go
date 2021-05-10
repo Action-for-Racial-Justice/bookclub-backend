639fe0cf287410e108f628829f89bd145690713f
kdcurrie <kdcurrie@dons.usfca.edu>
2021-05-10 13:09:41 -0700

    [unit] finished testing for error library

--- internal/bcerrors/bcerrors_test.go
+++ internal/bcerrors/bcerrors_test.go
@@ -1,23 +1,29 @@
 package bcerrors_test
 
 import (
+	"errors"
 	"testing"
 
-	error "github.com/Action-for-Racial-Justice/bookclub-backend/internal/bcerrors"
+	"github.com/Action-for-Racial-Justice/bookclub-backend/internal/bcerrors"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	expectedInternal   string = "type: Internal Server Error, message: This is not an internal error it's a test, root_cause: , external_message: "
-	expectedValidation string = "type: Validation Error, message: This is not a validation error it's a test, root_cause: , external_message: "
-	expectedDecode     string = "type: JSON Decode Error, message: This is not a decode error it's a test, root_cause: , external_message: "
+	expectedInternal       string = "type: Internal Server Error, message: This is not an internal error it's a test, root_cause: , external_message: "
+	expectedInternalExRoot string = "type: Internal Server Error, message: This is not an internal error it's a test, root_cause: internal root error, external_message: internal external message"
+
+	expectedValidation       string = "type: Validation Error, message: This is not a validation error it's a test, root_cause: , external_message: "
+	expectedValidationExRoot string = "type: Validation Error, message: This is not a validation error it's a test, root_cause: validation root error, external_message: validation external message"
+
+	expectedDecode       string = "type: JSON Decode Error, message: This is not a decode error it's a test, root_cause: , external_message: "
+	expectedDecodeExRoot string = "type: JSON Decode Error, message: This is not a decode error it's a test, root_cause: decode root error, external_message: decode external message"
 )
 
 func TestNewErrors(t *testing.T) {
 
-	errInternal := error.NewError("This is not an internal error it's a test", error.InternalError)
-	errValidation := error.NewError("This is not a validation error it's a test", error.ValidationError)
-	errDecode := error.NewError("This is not a decode error it's a test", error.DecodeError)
+	errInternal := bcerrors.NewError("This is not an internal error it's a test", bcerrors.InternalError)
+	errValidation := bcerrors.NewError("This is not a validation error it's a test", bcerrors.ValidationError)
+	errDecode := bcerrors.NewError("This is not a decode error it's a test", bcerrors.DecodeError)
 
 	assert.NotNil(t, errInternal)
 	assert.NotNil(t, errValidation)
@@ -26,4 +32,13 @@ func TestNewErrors(t *testing.T) {
 	assert.Equal(t, expectedInternal, errInternal.Error()[48:])
 	assert.Equal(t, expectedValidation, errValidation.Error()[48:])
 	assert.Equal(t, expectedDecode, errDecode.Error()[48:])
+
+	errInternal.WithExternalMessage("internal external message")
+	assert.Equal(t, expectedInternalExRoot, errInternal.WithRootCause(errors.New("internal root error")).Error()[48:])
+
+	errValidation.WithExternalMessage("validation external message")
+	assert.Equal(t, expectedValidationExRoot, errValidation.WithRootCause(errors.New("validation root error")).Error()[48:])
+
+	errDecode.WithExternalMessage("decode external message")
+	assert.Equal(t, expectedDecodeExRoot, errDecode.WithRootCause(errors.New("decode root error")).Error()[48:])
 }
